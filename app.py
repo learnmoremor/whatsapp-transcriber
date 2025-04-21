5ec91a29d1f4c25fe21d6f86bc17b8b9c49464d9
eli <matzvimatok@gmail.com>
2025-04-21 23:42:51 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask , request
+
+app = Flask(__name__)
+@app.route('/whatsapp/incoming' , methods=['POST'])
+def incoming_message():
+    data = request.from
+    print("Received whatsapp message:" , data)
+    
+
+    return "OK" , 200 
+
+if __name__ == '__main__':
+    app.run()
